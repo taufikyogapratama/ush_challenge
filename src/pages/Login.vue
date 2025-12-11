65865b8d4a56d74a034fa03be9080fd806541ba5
taufikyogapratama <taufikpratama326@gmail.com>
2025-12-12 06:53:09 +0700

    add: page login and register

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,86 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Ini Halaman Login</h1>
+  <section class="flex">
+    <div class="left w-[50vw] h-screen bg-cover flex flex-col justify-center">
+      <p class="font-satoshi font-bold text-5xl mb-7 text-center text-white">
+        Welcome to Rentverse
+      </p>
+      <p class="font-satosi font-semibold text-lg text-white text-center">
+        Realize your dream home. We craft spaces that are functional,<br />
+        inspiring joy, tranquility, and connection.
+      </p>
+    </div>
+    <div class="right w-[50vw] py-36">
+      <div class="w-[515px] mx-auto">
+        <h3
+          class="font-bold font-satoshi text-coklat text-3xl text-center mb-6"
+        >
+          Login Now
+        </h3>
+        <form>
+          <label
+            for="email"
+            class="font-satoshi font-bold text-lg text-gray-700 mt-3 block mb-1.5"
+            >Email</label
+          >
+          <input
+            type="email"
+            id="email"
+            placeholder="budi siregar@gmail.com"
+            class="w-full bg-orange py-2 px-5 rounded-lg"
+          /><br clear="mb-6" />
+
+          <label
+            for="password"
+            class="font-satoshi font-bold text-lg text-gray-700 block mb-1.5"
+            >Password</label
+          >
+          <input
+            type="password"
+            id="password"
+            placeholder="****************************"
+            class="w-full bg-orange py-2 px-5 rounded-lg"
+          /><br />
+          <label
+            for="confirmPassword"
+            class="font-satoshi font-bold text-lg text-gray-700 mt-3 block mb-1.5"
+            >Confirm Password</label
+          >
+          <input
+            type="confirmPassword"
+            id="confirmPassword"
+            placeholder="****************************"
+            class="w-full bg-orange py-2 px-5 rounded-lg mb-6"
+          /><br />
+          <input type="checkbox" value="agree" id="agree" required /><label
+            for="agree"
+            class="font-satoshi text-netral text-xl font-bold ml-3.5"
+            >By registering, I agree to
+            <span class="text-yellow hover:underline"
+              >Rentverse Terms &<br />
+              Conditions and Privacy Policy</span
+            ></label
+          ><br />
+          <input
+            type="submit"
+            value="Next"
+            class="py-2.5 w-full bg-yellow rounded-lg text-white mt-3.5 mb-6"
+          />
+          <p
+            class="font-satoshi text-netral text-xl font-bold ml-3.5 text-center"
+          >
+            Don't have a Rentverse account yet?
+            <RouterLink to="/register" class="text-yellow">Register</RouterLink>
+          </p>
+        </form>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+.left {
+  background-image: url("../assets/image_login.jpg");
+}
+</style>
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,5 +1,131 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Ini halaman register</h1>
+  <section class="flex">
+    <div class="left w-[50vw] h-screen bg-cover flex flex-col justify-center">
+      <p class="font-satoshi font-bold text-5xl mb-7 text-center text-white">
+        Welcome to Rentverse
+      </p>
+      <p class="font-satosi font-semibold text-lg text-white text-center">
+        Realize your dream home. We craft spaces that are functional,<br />
+        inspiring joy, tranquility, and connection.
+      </p>
+    </div>
+    <div class="right w-[50vw] py-10">
+      <div class="w-[515px] mx-auto">
+        <h3
+          class="font-bold font-satoshi text-coklat text-3xl text-center mb-6"
+        >
+          Register Now
+        </h3>
+        <form>
+          <label
+            for="fullname"
+            class="font-satoshi font-bold text-lg text-gray-700 block mb-1.5"
+            >Fullname</label
+          >
+          <input
+            type="text"
+            id="fullname"
+            placeholder="Budi Siregar"
+            class="w-full bg-orange py-2 px-5 rounded-lg"
+          /><br />
+          <label
+            for="email"
+            class="font-satoshi font-bold text-lg text-gray-700 mb-1.5"
+            >Email</label
+          >
+          <input
+            type="email"
+            id="email"
+            placeholder="budi siregar@gmail.com"
+            class="w-full bg-orange py-2 px-5 rounded-lg"
+          /><br />
+          <label
+            for="tenant"
+            class="font-satoshi font-bold text-lg text-gray-700 mt-3 mb-1.5"
+            >Role</label
+          >
+          <div class="flex justify-between">
+            <label
+              for="tenant"
+              class="py-2.5 px-5 border-2 border-gray-300 font-satoshi font-bold text-lg text-gray-700 rounded"
+              >Tenant
+              <input
+                type="radio"
+                id="tenant"
+                value="tenant"
+                class="ml-28"
+                name="role"
+                required
+            /></label>
+
+            <label
+              for="propertyOwner"
+              class="py-2.5 px-5 border-2 border-gray-300 font-satoshi font-bold text-lg text-gray-700 rounded"
+              >Property Owner
+              <input
+                name="role"
+                type="radio"
+                id="propertyOwner"
+                value="Property Owner"
+                class="ml-11"
+            /></label>
+          </div>
+
+          <p class="font-satoshi font-normal text-netral text-lg mt-1.5">
+            *Choose this if you are looking to search for and book an apartment
+          </p>
+          <label
+            for="password"
+            class="font-satoshi font-bold text-lg text-gray-700 mt-2 mb-1.5 block"
+            >Password</label
+          >
+          <input
+            type="password"
+            id="password"
+            placeholder="****************************"
+            class="w-full bg-orange py-2 px-5 rounded-lg"
+          />
+          <label
+            for="confirmPassword"
+            class="font-satoshi font-bold text-lg text-gray-700 mt-3 block mb-1.5"
+            >Confirm Password</label
+          >
+          <input
+            type="confirmPassword"
+            id="confirmPassword"
+            placeholder="****************************"
+            class="w-full bg-orange py-2 px-5 rounded-lg mb-4"
+          /><br />
+          <input type="checkbox" value="agree" id="agree" required /><label
+            for="agree"
+            class="font-satoshi text-netral text-xl font-bold ml-3.5"
+            >By registering, I agree to
+            <span class="text-yellow hover:underline"
+              >Rentverse Terms &<br />
+              Conditions and Privacy Policy</span
+            ></label
+          ><br />
+          <input
+            type="submit"
+            value="Next"
+            class="py-2.5 w-full bg-yellow rounded-lg text-white mt-3.5 mb-6"
+          />
+          <p
+            class="font-satoshi text-netral text-xl font-bold ml-3.5 text-center"
+          >
+            Already have a Rentverse account?
+            <RouterLink to="/login" class="text-yellow">Sign in</RouterLink>
+          </p>
+        </form>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+.left {
+  background-image: url("../assets/image_login.jpg");
+}
+</style>
