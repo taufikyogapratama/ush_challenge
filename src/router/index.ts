53f3e26a17f09a5fa90304042d4c2bdd6baa0447
taufikyogapratama <taufikpratama326@gmail.com>
2025-12-12 09:23:14 +0700

    fix: code at router/index

--- src/router/index.ts
+++ src/router/index.ts
@@ -34,7 +34,7 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _from, next) => {
   const isLogin = localStorage.getItem("isLogin");
 
   if (to.meta.requiresAuth && isLogin !== "true") {
